328984871ce5a08b8d6b15b1ef4466293804aed9
YousraEssam <yousraa.essam.15@hotmail.com>
2022-12-07 16:00:18 +0200

    create database using artisan command

--- app/Console/Commands/CreateDatabase.php
+++ app/Console/Commands/CreateDatabase.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\DB;
 
 class CreateDatabase extends Command
